a04f7eccd954bff98143365199a0b18cb93b23bd
Carl A. B. Pearson <pearsonca@users.noreply.github.com>
2025-06-26 12:56:17 +0200

    Update install_script.R

--- install_script.R
+++ install_script.R
@@ -1,26 +1,28 @@
 
-# prior to installing this, you will need to install `libsodium`
-# on unix systems, use apt, snap, etc to find libsodium and install
-# the dev version
-# for Windows / OSX, see https://py-ipv8.readthedocs.io/en/latest/preliminaries/install_libsodium.html
-# for installation advice
-# feel free to post on the discussion forums to request troubleshooting
-# help
-install.packages("sodium")
+# for general installation advice feel free to post on the discussion forums to request troubleshooting
+# help.
 
-install.packages("Matrix")
+# for this particular library, `sodium`, you may need to install `libsodium` manually. first try:
+install.packages("sodium")
+# if that doesn't work, you can manually install the library:
+# - on unix systems, use apt, snap, etc to find libsodium and install the dev version
+# - for Windows / OSX, see https://py-ipv8.readthedocs.io/en/latest/preliminaries/install_libsodium.html
 
 install.packages(c(
-	"rmarkdown", "ape", "epicontacts", "epitrix", "readxl", "tidyverse",
-	"remotes", "outbreaker2", "furrr", "coda", "o2ools", "mixtree",
-	"EpiEstim", "incidence", "ggpubr", "igraph", "tidygraph",
-	"ggraph", "incidence2"
+  "rmarkdown", "ape", "epicontacts", "epitrix", "readxl", "tidyverse",
+  "outbreaker2", "furrr", "coda", "o2ools", "mixtree",
+  "EpiEstim", "incidence", "ggpubr", "igraph", "tidygraph",
+  "ggraph", "incidence2", "remotes"
 ))
 
 # somewhat lagging versions of these are also available from CRAN
+# but we're going to install the latest versions from Github
+# you can expect to be prompted about updating dependencies, so you may need
+# to provide an interactive answer
 remotes::install_github("epiverse-trace/epiparameter")
 remotes::install_github("reconhub/distcrete")
 
-# not available on CRAN
+# these are not available on CRAN; again, you may be prompted about installing
+# dependencies
 remotes::install_github("mrc-ide/epireview")
 remotes::install_github("CyGei/linktree")
