54a0180fded0243edf33bae35b9be3b1e2026b19
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2025-06-26 06:06:26 -0400

    update install script

--- install_script.R
+++ install_script.R
@@ -1,8 +1,19 @@
 
+# prior to installing this, you will need to install `libsodium`
+# on unix systems, use apt, snap, etc to find libsodium and install
+# the dev version
+# for Windows / OSX, see https://py-ipv8.readthedocs.io/en/latest/preliminaries/install_libsodium.html
+# for installation advice
+# feel free to post on the discussion forums to request troubleshooting
+# help
+install.packages("sodium")
+
+install.packages("Matrix")
+
 install.packages(c(
 	"rmarkdown", "ape", "epicontacts", "epitrix", "readxl", "tidyverse",
 	"remotes", "outbreaker2", "furrr", "coda", "o2ools", "mixtree",
-	"parallel", "EpiEstim", "incidence", "ggpubr", "igraph", "tidygraph",
+	"EpiEstim", "incidence", "ggpubr", "igraph", "tidygraph",
 	"ggraph", "incidence2"
 ))
 
