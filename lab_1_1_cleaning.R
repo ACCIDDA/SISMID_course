67b1f3ec08fa30b5cb1e1050818fc47aba2611a4
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2025-07-09 21:00:41 -0400

    lab 1 scripts

---
+++ lab_1_1_cleaning.R
@@ -0,0 +1,39 @@
+
+# 1. load the raw_ebola_data, as in lab_1.qmd
+
+
+
+
+
+
+# 2. using `{cleanepi}` clean up the data enough to:
+#   - calculate the distribution of times from onset to sample ...
+#   - for females over age 20 ...
+#   - including where onset to sample is negative
+
+# hint: think what data are needed and what cleaning steps are necessary
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 3. perform the calculation and make a simple plot!
+
+
+
+
+
+
+
+
+
+# 4. BONUS: identify the unlikely values, systematically correct or exclude them, and make a new plot
