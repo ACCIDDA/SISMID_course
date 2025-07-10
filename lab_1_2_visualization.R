c2b2aba6a475a68764d41bbae8773b0f17be2d31
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2025-07-09 21:23:51 -0400

    lab 1 prac 2

---
+++ lab_1_2_visualization.R
@@ -0,0 +1,46 @@
+
+# 1. create a synthetic line list, sim_data, as in lab_1.qmd
+
+
+
+
+
+# 2. Aggregate `sim_data` by admission date, case outcome, and sex in 2-week windows (i.e. biweekly)
+
+
+
+
+
+
+
+
+# 3. Plot the biweekly incidence, faceted by outcome, and distinguishing by sex within panels
+
+
+
+# 4 BONUS: Polish up the plot; e.g. make the date break grid on months, relabel axes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 3. perform the calculation and make a simple plot!
+
+
+
+
+
+
+
+
+
+# 4. BONUS: identify the unlikely values, systematically correct or exclude them, and make a new plot
