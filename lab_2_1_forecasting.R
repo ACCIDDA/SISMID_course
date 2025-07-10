7d28a115b2c26eed34645a7a222582bc09b5eb3d
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2025-07-10 06:12:23 -0400

    lab 2 prac 1

---
+++ lab_2_1_forecasting.R
@@ -0,0 +1,22 @@
+
+# 1. load the ebola1976 data from the `{cfr}` package, as in lab_2.qmd, but redefine cases column as `primary` and deaths column as `secondary`.
+
+
+
+
+
+
+# 2. EpiNow2 has the capability to `estimate_secondary`;
+# investigate its use, then apply it to the Ebola data through Oct 1
+
+
+
+# 3. Use that estimate with the `forecast_secondary` capability, now providing the rest of the primary series
+
+
+
+
+
+
+
+# Hint: consult the examples from ?EpiNow2::estimate_secondary
