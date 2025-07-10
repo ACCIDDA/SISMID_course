280ccff89341fe4ef6b1f3c4f6ede8f367e0958a
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2025-07-10 07:00:17 -0400

    lab 2 prac 2

---
+++ lab_2_2_cfr.R
@@ -0,0 +1,24 @@
+
+# 1. load the data/sarscov2_cases_deaths.csv
+
+
+
+# 2. As in lab_2.qmd, calculate the rolling naive and adjusted CFRs for SARS-COV-2 (hint: you may need to adjust some column names).
+# For the onset to death distribution, use the values from Verity et al: https://www.medrxiv.org/content/10.1101/2020.03.09.20033357v1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 3. Plot them!
+
