b7c8f93d7382ea66ab8afdf5a6dd392dea447b5e
Carl A. B. Pearson <carl.pearson@lshtm.ac.uk>
2025-07-09 21:42:05 -0400

    final lab 1 exercise

---
+++ lab_1_3_growth.R
@@ -0,0 +1,18 @@
+
+# 1. load the UK covid data, as in lab_1.qmd, but this time do not filter it by date
+
+
+
+# 2. use `{incidence2}` to aggregate to daily counts
+
+
+
+# 3. Plot the data in way that you can identify two distinct periods,
+# one of decline and one of growth
+
+
+
+
+
+# 4. Extract those periods, then use `{i2extras}` to calculate growth during each
+
